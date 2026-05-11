ba776ed50f98387054d3a14f36f8998b2c2a1d93
Xingkai Wang <bittervan@zju.edu.cn>
2026-05-11 12:48:09 +0000

    finish matmul

--- softmax_attention/solve.cu
+++ softmax_attention/solve.cu
@@ -1,8 +1,53 @@
 #include <cuda_runtime.h>
 
-// A is M * N, B is N * K
-__global__ void matmul(const float* A, const float *B, int M, int N, int K) {
+__global__ void matmul_kernel(const float *A, const float *B, float *output, int M, int N, int K) {
+    __shared__ float s_A[16][16];
+    __shared__ float s_B[16][16];
+    __shared__ float s_C[16][16];
 
+    int x = blockDim.x * blockIdx.x + threadIdx.x;
+    int y = blockDim.y * blockIdx.y + threadIdx.y;
+
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    s_C[ty][tx] = 0;
+
+    __syncthreads();
+
+    for (int bk = 0; bk < K; bk += 16) {
+        int ax = bk * 16 + tx;
+        int by = bk * 16 + ty;
+
+        if (y < M && ax < K)
+            s_A[ty][tx] = A[y * K + ax];
+        else
+            s_A[ty][tx] = 0;
+
+        if (x < N && by < K)
+            s_B[ty][tx] = B[by * N + x];
+        else
+            s_B[ty][tx] = 0;
+
+        __syncthreads();
+
+        for (int i = 0; i < 16; i++) 
+            s_C[ty][tx] += s_A[ty][i] * s_B[i][tx];
+
+        __syncthreads();
+    }
+
+    if (y < M && x < N)
+        output[y * N + x] = s_C[ty][tx];
+    __syncthreads();
+}
+
+// A is M * N, B is N * K
+void matmul(const float* A, const float *B, float *output, int M, int N, int K) {
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((N + threadsPerBlock.x - 1) / threadsPerBlock.x, (M + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    matmul_kernel<<<blocksPerGrid, threadsPerBlock>>>(A, B, output, M, N, K);
 }
 
 // Q, K, V, output are device pointers
