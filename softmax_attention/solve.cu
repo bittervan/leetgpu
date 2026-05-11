6bc233821bb973dee201d7c31fff776987d9d2cb
Xingkai Wang <bittervan@zju.edu.cn>
2026-05-11 13:21:59 +0000

    now we have the transpose

--- softmax_attention/solve.cu
+++ softmax_attention/solve.cu
@@ -50,6 +50,36 @@ void matmul(const float* A, const float *B, float *output, int M, int N, int K) 
     matmul_kernel<<<blocksPerGrid, threadsPerBlock>>>(A, B, output, M, N, K);
 }
 
+__global__ void transpose_kernel(const float *input, float *output, int M, int N) {
+    __shared__ float s_data[16][16];
+    __shared__ float s_trans[16][17];
+
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    if (x < N && y < M) {
+        s_data[ty][tx] = input[y * N + x];
+    }
+    __syncthreads();
+    s_trans[tx][ty] = s_data[ty][tx];
+
+    int new_x = threadIdx.x + blockDim.y * blockIdx.y;
+    int new_y = threadIdx.y + blockDim.x * blockIdx.x;
+
+    if (new_x < M && new_y < N) {
+        output[new_y * M + new_x] = s_trans[ty][tx];
+    }
+}
+
+void transpose(const float *input, float *output, int M, int N) {
+    dim3 threadsPerBlock(16, 16);
+    dim3 blocksPerGrid((N + threadsPerBlock.x - 1) / threadsPerBlock.x, (M + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    transpose_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, M, N);
+}
+
 // Q, K, V, output are device pointers
 extern "C" void solve(const float* Q, const float* K, const float* V, float* output, int M, int N,
                       int d) {}
