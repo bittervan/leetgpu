c8715c3873186ec18eabf3507e8d89847529c71d
Xingkai Wang <bittervan@zju.edu.cn>
2026-05-11 09:15:49 +0000

    sync

--- softmax_attention/solve.cu
+++ softmax_attention/solve.cu
@@ -1,5 +1,10 @@
 #include <cuda_runtime.h>
 
+// A is M * N, B is N * K
+__global__ void matmul(const float* A, const float *B, int M, int N, int K) {
+
+}
+
 // Q, K, V, output are device pointers
 extern "C" void solve(const float* Q, const float* K, const float* V, float* output, int M, int N,
                       int d) {}
